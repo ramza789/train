44a0f984b8928ccdf919ae92390d184fbbce58ff
ramza789 <ramza789@hotmail.com>
2025-01-23 20:10:26 +0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,15 @@
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 12,
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
     center: { lat: 37.773972, lng: -122.431297 },
+    zoom: 12,
     keyboardShortcuts: false,
     streetViewControl: false,
     zoomControl: false,
   });
 }
+
+initMap();
